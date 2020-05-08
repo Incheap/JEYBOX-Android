ed420bba43c133851a9f9ee2c5ceed27d0d3dd49
Yannick Plourde <yann62plourde@gmail.com>
2020-05-08 15:11:20 -0400

    Added the UI's and the SQLite remaining

---
+++ app/src/main/java/com/sqlite/SQLiteManager.java
@@ -0,0 +1,12 @@
+package com.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SQLiteManager {
+
+}

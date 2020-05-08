f7f1b975520ec3cdb205d7dc5451f231a17666b3
Incheap <Incheap@users.noreply.github.com>
2020-05-08 12:29:39 -0400

    Created a menu
    Start of menu

--- app/src/main/java/com/example/jeybox/Accueil.java
+++ app/src/main/java/com/example/jeybox/Accueil.java
@@ -18,6 +18,9 @@ package com.example.jeybox;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,8 +33,34 @@ public class Accueil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
 
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         configureConsulterRes();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.logOut:
+                Intent intent1 = new Intent(this, Login.class);
+                this.startActivity(intent1);
+                return true;
+            case R.id.backAccueil:
+                Intent intent2 = new Intent(this, Accueil.class);
+                this.startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
 //  Navigation vers consulter_reservation
     private void configureConsulterRes(){
